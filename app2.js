3aec01158af0b2829e0dd069367564d4c771a736
SemihB <semihr5171@gmail.com>
2022-10-28 00:47:03 -0400

    Final Touches

--- app2.js
+++ app2.js
@@ -17,9 +17,16 @@ let askedCount = 0;
 let correctScore = askedCount;
 let totalQuestion = 18;
 
+// event listeners
+// function eventListeners(){
+//     $('#check-answer').on("click", checkAnswer);
+// }
+
+
 
 $(document).ready(function() {
     loadQuestion();
+//    eventListeners();
     $xForce.xtotalQuestion.text(totalQuestion);
     $xForce.xcorrectScore.text(correctScore);
 });
@@ -37,7 +44,7 @@ function loadQuestion(){
         (data) => {
             //questionData = data;
     showQuestion(data.results[0]);
-    selectOption(data.results[0])
+    selectOption(data.results[0]);
 })
 
 }
@@ -50,29 +57,41 @@ function showQuestion(data){
     // inserting correct answer in random position in the options list
     optionsList.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1)), 0, correctAnswer); 
     console.log(optionsList)
-    $xForce.xquestion.append(`${data.question} <br> <span class ="category">${data.category} </span>`);
-    $xForce.xoptions.append(`${optionsList.map((option, index) => `
+    $xForce.xquestion.html(`${data.question} <br> <span class ="category">${data.category} </span>`);
+    $xForce.xoptions.html(`${optionsList.map((option, index) => `
         <li> ${index + 1}. <span>${option}</span> </li>
         `).join('')}
     `);
     selectOption();
 }
-const oneLi = $('li').eq(0)
 // options selection
 function selectOption(data){
-    $('.quiz-options').each((option)=>{
-        $('li').on("click", () =>{
-            if($xForce.xoptions.toggleClass('.selected')){
-                const activeOption = $xForce.xoptions.toggleClass('.selected');
-                console.log(activeOption)
-                activeOption.removeClass('selected')
-            }
-            $('li').addClass('selected');
+    let clickedOn =  
+    // let correctAnswer = data.correct_answer;
+    // let incorrectAnswer = data.incorrect_answers;
+    // let optionsList = incorrectAnswer;
+    //let option = $('li span').val()
+    $('li').each((option)=>{
+        $('li .selected').on("click", () =>{
+            console.log("hello")
+            // if($('li .selected')){
+            //     const activeOption = $('.selected span');
+            //     activeOption.removeClass('selected');
+            // }
+            // $('li').addClass('selected');
         });
     });
 }
 
-
+// function checkAnswer(data){
+//     let correctAnswer = data.correct_answer;
+//     console.log(correctAnswer)
+//     $('#check-answer').attr("disabled", true);
+//     if($('.quiz-options .selected') === correctAnswer){
+//         let selectedAnswer = $('li .selected').text();
+//         console.log(selectedAnswer);
+//     } 
+// }
 
 
 
--- app3.js
+++ app3.js
@@ -1,21 +1,5 @@
 
-//let questionData;
 
-// const _question = document.getElementById('question');
-// const _options = document.querySelector('.quiz-options');
-// const _checkBtn = document.getElementById('check-answer');
-// const _playAgainBtn = document.getElementById('play-again');
-// const _result = document.getElementById('result');
-// const _correctScore = document.getElementById('correct-score');
-// const _totalQuestion = document.getElementById('total-question');
-
-// const $xquestion = $('#question');
-// const $xoptions = $('.quiz-options');
-// const $xcheckBtn = $('#check-answer');
-// const $xplayAgainBtn = $('#play-again');
-// const $xresult = $('#result');
-// const $xcorrectScore = $('#correct-score');
-// const $xtotalQuestion = $('#total-question');
 
 const $xForce = {
     xquestion: $('#question'),
@@ -28,10 +12,11 @@ const $xForce = {
 }
 
 let correctAnswer = "";
+let incorrectAnswer = "";
 let askedCount = 0;
 let correctScore = askedCount;
 let totalQuestion = 18;
-
+let finalScore = totalQuestion - correctScore;
 
 $(document).ready(function() {
     loadQuestion();
@@ -60,12 +45,12 @@ function showQuestion(data){
     let correctAnswer = data.correct_answer;
     let incorrectAnswer = data.incorrect_answers;
     let optionsList = incorrectAnswer;
-    console.log(data)
+    //console.log(data)
     // inserting correct answer in random position in the options list
     optionsList.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1)), 0, correctAnswer); 
     
-    $xForce.xquestion.append(`${data.question} <br> <span class ="category">${data.category} </span>`);
-    $xForce.xoptions.append(`${optionsList.map((option, index) => `
+    $xForce.xquestion.html(`${data.question} <br> <span class ="category">${data.category} </span>`);
+    $xForce.xoptions.html(`${optionsList.map((option, index) => `
     <li data-answer = "${correctAnswer}"> ${index + 1}.  ${option} </li>`).join('')}`);
     selectOption();
 }
@@ -74,24 +59,57 @@ function showQuestion(data){
 function selectOption(){
     $('.quiz-options').each(()=>{
         $('li').on("click", (e) =>{
-            //console.dir(e.target);
-            //console.log(e.target.innerText)
-            //console.log(e.target.parentElement.dataset)
-            //console.log(typeof e.target.innerText)
-            console.log(e.target.dataset.answer===e.target.innerText.split(" ").slice(1, e.target.innerText.split(" ").length).join(' '))
-
-            const clickedOption = e.target.innerText;
-            //if()
-//             if($('.quiz-options .selected')){
-//                 const activeOption = $('.quiz-options .selected');
-//                 activeOption.removeClass('selected');
-//             }
-//             $('.quiz-options .selected').ready(function(){
-//                 $('.quiz-options').addClass('selected');
-//             })
+            const clickedOption = e.target;
+            const correctAnswer = clickedOption.dataset.answer
+            let option = e.target.innerText.split(" ").slice(1, e.target.innerText.split(" ").length).join(' ')
+            //console.log(clickedOption.dataset.answer===clickedOption.innerText.split(" ").slice(1, clickedOption.innerText.split(" ").length).join(' '))
+            //console.log(correctAnswer)
+            //console.log(option)
+            if(correctAnswer === option){
+                $xForce.xcorrectScore.text(`${correctScore++}`);
+                $xForce.xcheckBtn.html(`<p><i class = "fas fa-check"></i>Correct Answer!</p>`)
+            }else{
+                $xForce.xcorrectScore.text(`${correctScore++}`);
+                $xForce.xcheckBtn.html(`<p><i class = "fas fa-times"></i>Incorrect Answer!</p> <small><b>Correct Answer: </b>${correctAnswer}</small>`)
+            }
+            checkCount();
+    
+        
             
         });
     })           
+}
+
+
+
+function checkCount(){
+    askedCount = askedCount +1;
+    setCount();
+    if(askedCount == totalQuestion){
+        setTimeout(() => {
+            console.log("");
+        }, 5000);
+
+        $xForce.xresult.html(`<p>Your score is ${correctScore}.</p>`);
+    } else {
+        setTimeout(() => {
+            loadQuestion();
+        }, 300);
+    }
+}
+
+function setCount(){
+    $xForce.xtotalQuestion.text(`${totalQuestion}`);
+    $xForce.xcorrectScore.textContent = correctScore;
+}
+
+function restartQuiz(){
+    correctScore = askedCount = 0;
+    $xForce.xplayAgainBtn.style.display = "none";
+    $xForce.xcheckBtn.style.display = "block";
+    $xForce.xcheckBtn.disabled = false;
+    setCount();
+    loadQuestion();
 }
 
 
